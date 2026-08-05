9c6842bc393dfccbfaa6bf3a7e0c7b40c3c680f8
1010524Alex <1010524@student.sae.edu.au>
2026-08-05 23:06:44 +1000

    Added Wall Texture and rewrote interact script for other Play Video Buttons

--- Assets/Scripts/DoorExit.cs
+++ Assets/Scripts/DoorExit.cs
@@ -2,49 +2,35 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
 
-public class DoorExit : MonoBehaviour
+public class DoorExit : MonoBehaviour, IInteractable
 {
-    public GameObject DoorExitPrefab;
-    public bool isWithinDoor;
-    public TextMeshProUGUI doorText;
+    
+    [SerializeField]
+    string objectInteractMessage;
+
+    public string interactMessage => objectInteractMessage;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        isWithinDoor = false;
-        doorText.enabled = false;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        exitGame();
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "Player")
-        {
-            isWithinDoor = true;
-            doorText.enabled = true;
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.tag == "Player")
-        {
-            isWithinDoor = false;
-            doorText.enabled = false;
-        }
+        
     }
 
     void exitGame()
     {
-        if (isWithinDoor == true && Input.GetKeyDown(KeyCode.E))
         {
             SceneManager.LoadScene(2);
         }
     }
 
-
+    public void Interact()
+    {
+        exitGame();
+    }
 }
