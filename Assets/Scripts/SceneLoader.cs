3157e0ab0c30180897391a217c5a8e48e5b573ce
1010524Alex <1010524@student.sae.edu.au>
2026-08-12 01:17:25 +1000

    Menus and Post Processing Implemented

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -4,13 +4,23 @@ public class SceneLoader : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void InstructionsMenu()
+    {
+        SceneManager.LoadScene(3);
     }
 
     public void QuitGame()
     {
         Application.Quit();
     }
+
 }
--- Assets/Scripts/TurnOnLight.cs
+++ Assets/Scripts/TurnOnLight.cs
@@ -3,16 +3,23 @@ using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.Timeline;
 
 public class TurnOnLight : MonoBehaviour
 {
-    public Bloom bloomSetting;
+    private Bloom bloomSetting;
+    private DepthOfField depthOfFieldSetting;
+    [SerializeField] private Volume postProcessingVolume;
     public GameObject light_1;
     public GameObject light_2;
     public GameObject darknessBox;
     public TextMeshProUGUI lightText;
     public bool isLightOn;
     public GameObject lightTextObject;
+    float elapsedTime;
+    public float speed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -21,18 +28,23 @@ public class TurnOnLight : MonoBehaviour
         lightText.enabled = true;
         light_1.SetActive(false);
         light_2.SetActive(false);
-        bloomSetting = GetComponent<Bloom>();
-        bloomSetting.intensity.value = 0;
+        postProcessingVolume.profile.TryGet(out bloomSetting);
+        postProcessingVolume.profile.TryGet(out depthOfFieldSetting);
+        bloomSetting.intensity.value = 100;
+        depthOfFieldSetting.focalLength.value = 300;
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        elapsedTime += Time.deltaTime;
+
         if (Input.GetKeyDown(KeyCode.E))
         {
             switchLightOn();
             isLightOn = true;
+            StartCoroutine(EyesAdjust());
         }
 
         if (isLightOn == true)
@@ -47,6 +59,12 @@ public class TurnOnLight : MonoBehaviour
         light_1.SetActive(true);
         light_2.SetActive(true);
         darknessBox.SetActive(false);
-        bloomSetting.intensity.value = 100;
+    }
+
+    IEnumerator EyesAdjust()
+    {
+        yield return new WaitForSeconds(1);
+        bloomSetting.intensity.value = Mathf.Lerp(100f, 3.73f, speed * elapsedTime);
+        depthOfFieldSetting.focalLength.value = Mathf.Lerp(300f, 0, speed * elapsedTime);
     }
 }
